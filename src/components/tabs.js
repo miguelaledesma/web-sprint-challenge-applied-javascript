3f1bdd7453dc053163478508baf17e4645e82d14
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-02-10 22:13:40 -0800

    met mvp

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -36,16 +36,63 @@ const Tabs = (topics) => {
 
  });
  return topicsDiv; 
+
+
+// const divTopics = document.createElement('div');
+//   divTopics.classList.add('topics');
+//   const newTab = document.createElement('div');
+//   newTab.classList.add('tab');
+
+//   newTab.textContent = `${topics}`
+
+//   divTopics.appendChild(newTab)
+
+//   return divTopics
 }
 
 const tabsAppender = (selector) => {
-  // TASK 4
+  
+axios.get('http://localhost:5000/api/topics').then(resp => {
+  console.log(resp); 
+  const tabsNav = document.querySelector(selector)
+  const topic = resp.data.topics
+  const newNavTab = Tabs(topic)
+  tabsNav.appendChild(newNavTab);  
+})
+
+
+
+.catch(err => {
+  console.error(err)
+})
+.finally(()=>{
+  console.log('it worked!')
+})
+
+// const entryPoint = document.querySelector('.tabs-container')
+//   axios.get(`http://localhost:5000/api/topics`)
+// .then(resp => {
+//   const objects = resp.data.topics
+//   objects.forEach(object => {
+//     const newsTab = Tabs(object)
+//     console.log(newsTab)
+//     entryPoint.append(newsTab)
+//   })
+// })
+// .catch(err =>{
+//   console.log(err);
+// })
+
+
+}
+
+
+// TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
   // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it with a console.log!).
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-}
 
 export { Tabs, tabsAppender }
